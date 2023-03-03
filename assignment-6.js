d45820c04755c5f5280e22e37c608f5de2468584
Md.Mohi Uddin <saddammd203@gmail.com>
2023-03-04 01:30:40 +0600

    add modal

--- assignment-6.js
+++ assignment-6.js
@@ -5,24 +5,34 @@ const loadData= async()=>{
    displayData(data.data.tools);
     
 }
+
 const displayData =tools =>{
    const allDataContainer =document.getElementById('all-card-container');
-   tools=tools.slice(0,6)
+
+  tools=tools.slice(0,6)
    tools.forEach(tool =>{
-    
     const dataDiv = document.createElement('div');
    dataDiv.classList.add('col');
    dataDiv.innerHTML=` <div class="card p-4">
    <img src="${tool.image}" class="card-img-top" alt="...">
    <div class="card-body">
+   <h3>features</h3>
      <h5 class="card-title">${tool.name}</h5>
-     <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+     <p class="card-text">${tool.description}</p>
+     
+  <p class="card-text">${tool.published_in}</p>
+     <img src="image/arrow_forward.png" alt="">
+     
+
    </div>
  </div>
  `;
  allDataContainer.appendChild(dataDiv);
    })
+
 }
+
+
 
 const toggleSpinner = isLoading => {
     const loadingSection =document.getElementById('Loading');
@@ -31,4 +41,8 @@ const toggleSpinner = isLoading => {
         loadingSection.classList.remove('d-none')
     }
 }
+
+
+
+
 loadData();
