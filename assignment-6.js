bdaef617d74c138aa75f46ee5387a1d0af202443
Md.Mohi Uddin <saddammd203@gmail.com>
2023-03-03 20:58:42 +0600

    added cards

---
+++ assignment-6.js
@@ -0,0 +1,25 @@
+const loadData= async()=>{
+    const url=`https://openapi.programming-hero.com/api/ai/tools`
+    const res =await fetch(url);
+    const data =await res.json();
+   displayData(data.data.tools);
+    
+}
+const displayData =tools =>{
+   const allDataContainer =document.getElementById('all-card-container');
+   tools.forEach(tool =>{
+    
+    const dataDiv = document.createElement('div');
+   dataDiv.classList.add('col');
+   dataDiv.innerHTML=` <div class="card p-4">
+   <img src="${tool.image}" class="card-img-top" alt="...">
+   <div class="card-body">
+     <h5 class="card-title">${tool.name}</h5>
+     <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+   </div>
+ </div>
+ `;
+ allDataContainer.appendChild(dataDiv);
+   })
+}
+loadData();
