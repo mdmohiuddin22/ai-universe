961a5932953abb2a879c073fba129ce62ac846cf
Md.Mohi Uddin <saddammd203@gmail.com>
2023-03-04 21:58:23 +0600

    modal update

--- assignment-6.js
+++ assignment-6.js
@@ -22,7 +22,7 @@ const displayData =tools =>{
      <div class="d-flex justify-content-between ">  <p class="card-text">${tool.published_in}</p>
      
      <!-- trigger modal -->
-     <div  onclick  type="button"  data-bs-toggle="modal" data-bs-target="#cardDetails">
+     <div id="modalclk" onclick="loadCardDetail('${tool.id}')"  type="button"  data-bs-toggle="modal" data-bs-target="#cardDetails">
      <i  id="arrow" class="fa-solid fa-arrow-right"></i></div>
      </div>
 
@@ -43,8 +43,26 @@ const toggleSpinner = isLoading => {
         loadingSection.classList.remove('d-none')
     }
 }
-const loadCardDetail = cardDetails=>{
 
+const loadCardDetail =(id)=>{
+  const URL = `https://openapi.programming-hero.com/api/ai/tool/${id}`;
+  fetch(URL)
+  .then((res)=>res.json())
+  .then((data)=> (data));
+};
+// const loadCardDetail= async()=>{
+//   // const url=`https://openapi.programming-hero.com/api/ai/tool/${id}`
+//   // const res =await fetch(url);
+//   // const data =await res.json();
+//   // displayCardDetails(data.data.tools[0]);
+  
+  
+// }
+
+const displayCardDetails =data =>{
+  // document.getElementById('cardDetailsLabel').innerText=data;
+  const cardDetailsBody =document.getElementById('modal-body');
+  cardDetailsBody.innerHTML=`<img src="${tool.image}" class="card-img-top" alt="...">`;
+  
 }
-
 loadData();
